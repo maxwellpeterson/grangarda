31323b039749340f71a1058808fb8aac10bd256e
Max Peterson <mpeterson@cloudflare.com>
2026-03-20 16:27:00 -0700

    Fail build if VITE_MAPBOX_TOKEN is not set

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+if (!process.env.VITE_MAPBOX_TOKEN) {
+  throw new Error("VITE_MAPBOX_TOKEN is required")
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
